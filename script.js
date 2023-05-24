36c2fb368c8da0d139ea811ecaa8c7e823533b24
vishalpawar132 <133097620+vishalpawar132@users.noreply.github.com>
2023-05-24 14:57:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+    class Selector {
+      applyStyling() {
+        const paragraphs = document.querySelectorAll('p');
+        const selectedParagraphs = [paragraphs[0], paragraphs[2]];
+
+        selectedParagraphs.forEach(paragraph => {
+          paragraph.classList.add('selector');
+        });
+      }
+    }
+
+    const selector = new Selector();
+    selector.applyStyling();
